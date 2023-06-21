0b01265aed1fdb79c0a993ce22587bade7ff0751
Nicholas Tsim <nicholastsim@gmail.com>
2023-06-21 02:57:42 +0100

    Remove unneeded data set

--- src/data/app-subject-summary-202122-q4/load.sql
+++
@@ -1,5 +0,0 @@
-COPY indicators FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/app-subject-summary-202122-q4/indicators.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
-COPY filters FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/app-subject-summary-202122-q4/filters.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
-COPY locations FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/app-subject-summary-202122-q4/locations.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
-COPY time_periods FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/app-subject-summary-202122-q4/time_periods.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
-COPY "data" FROM '/home/nick/Development/personal/ees-public-api-mock/src/data/app-subject-summary-202122-q4/data.parquet' (FORMAT 'parquet', CODEC 'ZSTD');
