b432c631326c896c132d89392ecb00925612712d
Nicholas Tsim <nicholastsim@gmail.com>
2023-12-08 12:21:18 +0000

    Add more publication details to PublicationSummaryViewModel

--- src/mocks/publications.ts
+++ src/mocks/publications.ts
@@ -4,27 +4,41 @@ import { PublicationSummaryViewModel } from '../schema';
 export const spcPublication = createPublication({
   id: '638482b6-d015-4798-a1a4-e2311253b3e1',
   title: 'Schools, pupils and their characteristics',
+  summary:
+    'School and pupil statistics for England including age, gender, free school meals (FSM), ethnicity, English as additional language (EAL), class size.',
+  lastPublished: '2023-01-01T12:00:00',
 });
 
 export const pupilAbsencePublication = createPublication({
   id: '9676af6b-d563-41f4-d071-08da8f468680',
   title: 'Pupil attendance in schools',
+  summary:
+    'Pupil absence, including overall, authorised and unauthorised absence and persistent absence by reason and pupil characteristics for the full academic year.',
+  lastPublished: '2023-01-01T12:00:00',
 });
 
 export const leoPublication = createPublication({
   id: 'b329f8e4-4191-4f0c-66a8-08d9daa3b093',
   title: 'LEO Graduate and Postgraduate Outcomes',
+  summary:
+    'Earnings and employment for higher education university graduates & postgraduates by subject studied and characteristics. Longitudinal Education Outcomes (LEO)..',
+  lastPublished: '2023-01-01T12:00:00',
 });
 
 export const apprenticeshipsPublication = createPublication({
   id: 'cf0ec981-3583-42a5-b21b-3f2f32008f1b',
   title: 'Apprenticeships and traineeships',
+  summary:
+    'Apprenticeship and traineeship starts, achievements and participation. Includes breakdowns by age, sex, ethnicity, subject, provider, geography etc.',
+  lastPublished: '2023-01-01T12:00:00',
 });
 
 // Don't put this in allPublications to keep it hidden
 export const benchmarkPublication = createPublication({
   id: '1681557f-510f-446e-bc9a-f2c7a59d1cfa',
   title: 'Benchmarking',
+  summary: 'N/A',
+  lastPublished: '2023-01-01T12:00:00',
 });
 
 export const allPublications: PublicationSummaryViewModel[] = [
